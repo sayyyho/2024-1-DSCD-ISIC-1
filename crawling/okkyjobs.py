fbb1eb1d9cb9a32534b17c560fddd5d3a2f38597
jongwon <hjw6207@gmail.com>
2024-04-11 16:31:08 +0900

    M

--- crawling/okkyjobs.py
+++ crawling/okkyjobs.py
@@ -1,10 +1,17 @@
+from bs4 import BeautifulSoup
+import time
 from selenium import webdriver
-from selenium.webdriver import ActionChains
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 import time
 
 driver = webdriver.Chrome()
-okky_url = "https://jobs.okky.kr/talents?page=1"
+okky_url = 'https://jobs.okky.kr/talents'
 driver.get(okky_url)
 time.sleep(2)
 
@@ -13,19 +20,27 @@ time.sleep(2)
 # element1 = dr.find_element_by_css_selector('선택자')  # 동작 할 요소 선택
 # act.click(element1).perform()  # element1  클릭 동작을 수행
 
-lst = []
+sen_lst = []
+skills_lst = []
 # XPath를 찾아서 리스트로 저장합니다.
-elements = driver.find_elements_by_xpath("/html/body/div/main/div/div[3]/div[2]/button[1]/div/div[2]/div[1]")
+# sentence = driver.find_element(By.XPATH ,'//*[@id="__next"]/main/div/div[3]/div[2]/button[1]/div/div[2]/div[1]')
+# print(elements.text)
 
-# 결과를 담을 빈 리스트를 만듭니다.
-xpath_list = []
+for i in range(1,21):
+    sentence = driver.find_element(By.XPATH, '//*[@id="__next"]/main/div/div[3]/div[2]/button[' + str(i) + ']/div/div[2]/div[1]')
+    sen_lst.append(sentence.text)
 
-# 각 요소의 텍스트를 가져와 리스트에 추가합니다.
-for element in elements:
-    xpath_list.append(element.text)
+    xpath = '//*[@id="__next"]/main/div/div[3]/div[2]/button[' + str(i) + ']/div/'
+    sentence = driver.find_element(By.XPATH, xpath + 'div[2]/div[1]')
+    sen_lst.append(sentence.text)
 
-# 리스트 출력
-print(xpath_list)
+    # skills = driver.find_element(By.XPATH, xpath + 'div[3]/div[1]/span')
+    # for skill in skills:
+    #     skills_lst.append(skill.text)
 
-# Selenium 세션 종료
-driver.quit()
+    for j in range(1,6):
+        skills = driver.find_element(By.XPATH, xpath + 'div[3]/div[1]/span[' + str(j) + ']')
+        skills_lst.append(skills.text)
+
+print(sen_lst)
+print(skills_lst)
