5100a6ba4776c819169f477ac18399e5fae20cbd
Choijian <an032944@daum.net>
2024-05-01 13:50:53 +0900

    링커리어 크롤링 및 분석

---
+++ linkareer_crawling/linkareer_total.py
@@ -0,0 +1,27 @@
+# linkareer_total.py
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+import linkareer
+import pandas as pd
+
+url=".\linkareer_link.txt"
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+df = pd.DataFrame(columns=['info', 'specification', 'self_intro'])
+
+#linkareer.url_crawl(driver=driver)
+f=open(url,'r')
+while True:
+    txt_link = f.readline()
+    if txt_link=="":
+        break
+    person = linkareer.self_introduction(driver=driver,url=txt_link)
+    df = pd.concat([df,person])
+    
+    df.to_csv('output.csv', encoding='utf-8')
+    
+driver.close()
+f.close()
+
