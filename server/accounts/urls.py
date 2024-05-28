d3b02a092004485cc1c228349f0af5d750fff426
Choijian <an032944@daum.net>
2024-05-28 19:39:54 +0900

    Feat: 사용자 프로필 구현

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path, include
+from .views import ProfileViewSet
+from rest_framework import routers
+
+
+profile_list = ProfileViewSet.as_view({
+    'get': 'list',
+    'post': 'create',
+    'patch': 'partial_update'
+})
 
 urlpatterns = [
-    path('', include('dj_rest_auth.urls')),
-    path('registration', include('dj_rest_auth.registration.urls')),
+    path('profile/', profile_list, name='profile-list'),
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
 ]
