571fa1b46e73952ead04a14a49f593f03d382206
Choijian <an032944@daum.net>
2024-05-30 17:39:55 +0900

    Feat: matching 앱 db 설계

--- server/matching/models.py
+++ server/matching/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Senior_Profile(models.Model):
+    job = models.CharField(max_length=20)
+    major = models.CharField(max_length=20)
+    double_major = models.CharField(max_length=20)
+    sex = models.CharField(max_length=4)
+    grades = models.CharField(max_length=20)
+    skills = models.CharField(max_length=100)
+    award_part = models.CharField(max_length=100)
+    club_part = models.CharField(max_length=100)
+    project_part = models.CharField(max_length=100)
