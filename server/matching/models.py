cc93eabee7094ec2143ab92f3dc46748958067e9
Choijian <an032944@daum.net>
2024-05-31 17:29:04 +0900

    Fix: 선배 정보 복수전공-> null값 허용

--- server/matching/models.py
+++ server/matching/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Senior_Profile(models.Model):
     job = models.CharField(max_length=20)
     major = models.CharField(max_length=20)
-    double_major = models.CharField(max_length=20)
+    double_major = models.CharField(max_length=20, blank=True)
     sex = models.CharField(max_length=4)
     grades = models.CharField(max_length=20)
     skills = models.CharField(max_length=100)
