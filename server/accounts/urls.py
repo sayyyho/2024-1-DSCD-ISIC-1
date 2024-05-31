0434ada32e45306a25a4d0826f381a3160920329
Choijian <an032944@daum.net>
2024-05-31 17:26:57 +0900

    Fix: 회원가입 url 재수정

--- server/accounts/urls.py
+++ server/accounts/urls.py
@@ -10,7 +10,7 @@ profile_list = ProfileViewSet.as_view({
 })
 
 urlpatterns = [
+    path('', include('dj_rest_auth.urls')),
     path('profile/', profile_list, name='profile-list'),
-    path('auth/', include('dj_rest_auth.urls')),
-    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('registration', include('dj_rest_auth.registration.urls')),
 ]
