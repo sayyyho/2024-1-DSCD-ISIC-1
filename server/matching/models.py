6916abc8c010ac1aaca5b6ee2438e0d16c780a21
Choijian <an032944@daum.net>
2024-06-01 18:22:35 +0900

    Feat: 임베딩된 선배 정보 저장해주는 model 생성

--- server/matching/models.py
+++ server/matching/models.py
@@ -10,3 +10,13 @@ class Senior_Profile(models.Model):
     award_part = models.CharField(max_length=100)
     club_part = models.CharField(max_length=100)
     project_part = models.CharField(max_length=100)
+    
+class Embedded_Senior_Profile(models.Model):
+    senior_info = models.OneToOneField(Senior_Profile, on_delete=models.CASCADE)
+    major_embedding = models.JSONField(blank=True)
+    double_major_embedding = models.JSONField(blank=True)
+    grades_embedding = models.JSONField(blank=True)
+    skills_embedding = models.JSONField(blank=True)
+    award_part_embedding = models.JSONField(blank=True)
+    club_part_embedding = models.JSONField(blank=True)
+    project_part_embedding = models.JSONField(blank=True)
