37528e4d36ea1968f924a0318ad7711f9e9ce474
sayyyho <323psh@naver.com>
2024-06-03 23:39:27 +0900

    Feat: react-select 도입 및 구현

--- client/src/pages/Info/index.tsx
+++ client/src/pages/Info/index.tsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
+import Select, { StylesConfig } from "react-select";
 import { useNavigate } from "react-router-dom";
 import { PageLayout } from "@/components/PageLayout";
 import { Header } from "@/components/common/Header";
 import { Wrapper } from "@/components/common/Wrapper";
 import { Text } from "@/components/common/Text";
 import { Input } from "@/components/common/Input";
-import { Select } from "@/components/common/Select";
+import { skills } from "@/constant/options";
+import { grades } from "@/constant/options";
+import { fileds } from "@/constant/options";
 import { TextArea } from "@/components/common/TextArea";
+import { Option } from "@/constant/options";
+import { Button } from "@/components/common/Button";
 import BACK from "@/assets/images/back.svg";
 
 export const Info = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
   const navigate = useNavigate();
+
+  const customStyles: StylesConfig<Option, false> = {
+    container: (provided) => ({
+      ...provided,
+      width: "100%",
+    }),
+  };
+
   return (
     <PageLayout $justifyContent="start">
       <Header>
@@ -57,19 +72,29 @@ export const Info = () => {
           학점
         </Text>
         <Select
-          width="100%"
-          height="40px"
-          $dropType="grade"
-          $radius="6px"
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={grades}
+          styles={customStyles}
         ></Select>
+        <Text color="black" size="16px" $selfProps="flex-start">
+          보유기술
+        </Text>
+        <Select
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={skills}
+          styles={customStyles}
+        ></Select>
+
         <Text color="black" size="16px" $selfProps="flex-start">
           수상이력
         </Text>
         <Select
-          width="100%"
-          height="40px"
-          $dropType="part"
-          $radius="6px"
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={fileds}
+          styles={customStyles}
         ></Select>
         <TextArea
           width="98%"
@@ -81,10 +106,10 @@ export const Info = () => {
           동아리 활동
         </Text>
         <Select
-          width="100%"
-          height="40px"
-          $dropType="part"
-          $radius="6px"
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={fileds}
+          styles={customStyles}
         ></Select>
         <TextArea
           width="98%"
@@ -96,10 +121,10 @@ export const Info = () => {
           프로젝트 경험
         </Text>
         <Select
-          width="100%"
-          height="40px"
-          $dropType="part"
-          $radius="6px"
+          defaultValue={selectedOption}
+          onChange={setSelectedOption}
+          options={fileds}
+          styles={customStyles}
         ></Select>
         <TextArea
           width="98%"
@@ -107,6 +132,16 @@ export const Info = () => {
           defaultString="세부 설명을 입력해주세요."
           $radius="6px"
         ></TextArea>
+        <Button
+          margin="20px 0px 0px 0px"
+          width="100%"
+          height="50px"
+          backgroundColor="#4D3E3E"
+          radius="5px"
+          color="white"
+        >
+          저장하기
+        </Button>
       </Wrapper>
     </PageLayout>
   );
