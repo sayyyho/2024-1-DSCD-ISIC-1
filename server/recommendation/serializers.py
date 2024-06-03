c8554c27cc6db18bf3211de7ce8d64ccdda6fe18
Choijian <an032944@daum.net>
2024-06-03 22:33:25 +0900

    Fix: 직업 추천 시 학점은 제외

--- server/recommendation/serializers.py
+++ server/recommendation/serializers.py
@@ -4,5 +4,5 @@ from accounts.models import Profile
 class JobRecommendSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
-        fields = ['major', 'double_major', 'grades', 'skills', 'award_detail', 'club_detail', 'project_detail']
+        fields = ['major', 'double_major', 'skills', 'award_detail', 'club_detail', 'project_detail']
         
