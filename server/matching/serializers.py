1ba821de70951010b94d88d81220ff62b79d7e0e
Choijian <an032944@daum.net>
2024-06-04 20:53:06 +0900

    Fix: 선배 스킬들 리스트 형식으로 반환

--- server/matching/serializers.py
+++ server/matching/serializers.py
@@ -8,9 +8,16 @@ class SeniorRecommendSerializer(serializers.ModelSerializer):
         fields = ['major', 'double_major', 'grades', 'skills', 'award_part', 'club_part', 'project_part']
 
 class SeniorProfileSerializer(serializers.ModelSerializer):
+    skills = serializers.SerializerMethodField()
+    
     class Meta:
         model = Senior_Profile
         fields = '__all__'
+        
+    def get_skills(self, obj):
+        if obj.skills:
+            return [skill.strip() for skill in obj.skills.split(',')]
+        return []
         
 class EmbeddedSeniorProfileSerializer(serializers.ModelSerializer):
     senior_info = SeniorProfileSerializer()
